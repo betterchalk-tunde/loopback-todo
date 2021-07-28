85968fbd9c45ce8a46f7da708ffc57b40b1fdc28
betterchalk-tunde <tunde@betterchalk.com>
2021-07-28 19:10:13 +0000

    Added Authorization with Jwt

--- loopback-todo/src/application.ts
+++ loopback-todo/src/application.ts
@@ -1,16 +1,24 @@
-import {BootMixin} from '@loopback/boot';
-import {ApplicationConfig} from '@loopback/core';
+import { BootMixin } from '@loopback/boot';
+import { ApplicationConfig } from '@loopback/core';
 import {
   RestExplorerBindings,
   RestExplorerComponent,
 } from '@loopback/rest-explorer';
-import {RepositoryMixin} from '@loopback/repository';
-import {RestApplication} from '@loopback/rest';
-import {ServiceMixin} from '@loopback/service-proxy';
+import { RepositoryMixin } from '@loopback/repository';
+import { RestApplication } from '@loopback/rest';
+import { ServiceMixin } from '@loopback/service-proxy';
 import path from 'path';
-import {MySequence} from './sequence';
+import { MySequence } from './sequence';
+import { AuthenticationComponent } from '@loopback/authentication';
+import {
+  JWTAuthenticationComponent,
+  SECURITY_SCHEME_SPEC,
+  UserServiceBindings,
+} from '@loopback/authentication-jwt';
+import { DbDataSource } from './datasources';
 
-export {ApplicationConfig};
+
+export { ApplicationConfig };
 
 export class LoopbackTodoApplication extends BootMixin(
   ServiceMixin(RepositoryMixin(RestApplication)),
@@ -30,6 +38,11 @@ export class LoopbackTodoApplication extends BootMixin(
     });
     this.component(RestExplorerComponent);
 
+    this.component(AuthenticationComponent);
+    // Mount jwt component
+    this.component(JWTAuthenticationComponent);
+    // Bind datasource
+    this.dataSource(DbDataSource, UserServiceBindings.DATASOURCE_NAME);
     this.projectRoot = __dirname;
     // Customize @loopback/boot Booter Conventions here
     this.bootOptions = {
