909366a09d3378a5aff9de18a21de71cdea684c2
betterchalk-tunde <tunde@betterchalk.com>
2021-07-28 15:21:09 +0000

    Added Todolist and able to create todo in a todolist

--- loopback-todo/src/controllers/todo.controller.ts
+++ loopback-todo/src/controllers/todo.controller.ts
@@ -17,19 +17,19 @@ import {
   requestBody,
   response,
 } from '@loopback/rest';
-import {Todo} from '../models';
-import {TodoRepository} from '../repositories';
+import { Todo, TodoList } from '../models';
+import { TodoRepository } from '../repositories';
 
 export class TodoController {
   constructor(
     @repository(TodoRepository)
-    public todoRepository : TodoRepository,
-  ) {}
+    public todoRepository: TodoRepository,
+  ) { }
 
   @post('/todos')
   @response(200, {
     description: 'Todo model instance',
-    content: {'application/json': {schema: getModelSchemaRef(Todo)}},
+    content: { 'application/json': { schema: getModelSchemaRef(Todo) } },
   })
   async create(
     @requestBody({
@@ -50,7 +50,7 @@ export class TodoController {
   @get('/todos/count')
   @response(200, {
     description: 'Todo model count',
-    content: {'application/json': {schema: CountSchema}},
+    content: { 'application/json': { schema: CountSchema } },
   })
   async count(
     @param.where(Todo) where?: Where<Todo>,
@@ -58,14 +58,17 @@ export class TodoController {
     return this.todoRepository.count(where);
   }
 
-  @get('/todos')
-  @response(200, {
-    description: 'Array of Todo model instances',
-    content: {
-      'application/json': {
-        schema: {
-          type: 'array',
-          items: getModelSchemaRef(Todo, {includeRelations: true}),
+  @get('/todos', {
+    responses: {
+      '200': {
+        description: 'Array of Todo model instances',
+        content: {
+          'application/json': {
+            schema: {
+              type: 'array',
+              items: getModelSchemaRef(Todo, { includeRelations: true }),
+            },
+          },
         },
       },
     },
@@ -79,13 +82,13 @@ export class TodoController {
   @patch('/todos')
   @response(200, {
     description: 'Todo PATCH success count',
-    content: {'application/json': {schema: CountSchema}},
+    content: { 'application/json': { schema: CountSchema } },
   })
   async updateAll(
     @requestBody({
       content: {
         'application/json': {
-          schema: getModelSchemaRef(Todo, {partial: true}),
+          schema: getModelSchemaRef(Todo, { partial: true }),
         },
       },
     })
@@ -95,18 +98,21 @@ export class TodoController {
     return this.todoRepository.updateAll(todo, where);
   }
 
-  @get('/todos/{id}')
-  @response(200, {
-    description: 'Todo model instance',
-    content: {
-      'application/json': {
-        schema: getModelSchemaRef(Todo, {includeRelations: true}),
+  @get('/todos/{id}', {
+    responses: {
+      '200': {
+        description: 'Todo model instance',
+        content: {
+          'application/json': {
+            schema: getModelSchemaRef(Todo, { includeRelations: true }),
+          },
+        },
       },
     },
   })
   async findById(
     @param.path.number('id') id: number,
-    @param.filter(Todo, {exclude: 'where'}) filter?: FilterExcludingWhere<Todo>
+    @param.filter(Todo, { exclude: 'where' }) filter?: FilterExcludingWhere<Todo>
   ): Promise<Todo> {
     return this.todoRepository.findById(id, filter);
   }
@@ -120,7 +126,7 @@ export class TodoController {
     @requestBody({
       content: {
         'application/json': {
-          schema: getModelSchemaRef(Todo, {partial: true}),
+          schema: getModelSchemaRef(Todo, { partial: true }),
         },
       },
     })
@@ -146,5 +152,22 @@ export class TodoController {
   })
   async deleteById(@param.path.number('id') id: number): Promise<void> {
     await this.todoRepository.deleteById(id);
+  }
+  @get('/todos/{id}/todo-list', {
+    responses: {
+      '200': {
+        description: 'TodoList belonging to Todo',
+        content: {
+          'application/json': {
+            schema: {type: 'array', items: getModelSchemaRef(TodoList)},
+          },
+        },
+      },
+    },
+  })
+  async getTodoList(
+    @param.path.number('id') id: typeof Todo.prototype.id,
+  ): Promise<TodoList> {
+    return this.todoRepository.todoList(id);
   }
 }
